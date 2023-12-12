fc611747b78fff7d16f8a8c62b3d6bce82ffc373
arjunmovate <153610050+arjunmovate@users.noreply.github.com>
2023-12-12 12:37:28 +0000

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -2,6 +2,6 @@ class helloworld
 {
   public static void main(String args[])
 {
-  System.out.println("New program on 12th Dec 2023");
+  System.out.println("New program on 12th Dec 2023 6:07 PM");
 }
 }
